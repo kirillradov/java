626c4b6d6244262a5597b4ee6bbbc0f506269872
kirillradov <46073708+kirillradov@users.noreply.github.com>
2020-05-23 20:43:42 +0300

    lesson1

---
+++ lesson1/src/main/java/ru/geekbrains/lesson1/FirstApp.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.lesson1;
+
+    public class FirstApp {
+        public static void main(String[] args) {
+            System.out.println("Hello, World!");
+            int a = 10;
+        }
+    }
+
