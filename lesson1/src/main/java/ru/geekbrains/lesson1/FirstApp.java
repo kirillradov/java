5771a786710846e53b7b0d9865dd638a3be63714
kirillradov <46073708+kirillradov@users.noreply.github.com>
2020-05-23 21:26:05 +0300

    Update FirstApp.java

--- lesson1/src/main/java/ru/geekbrains/lesson1/FirstApp.java
+++ lesson1/src/main/java/ru/geekbrains/lesson1/FirstApp.java
@@ -3,7 +3,26 @@ package ru.geekbrains.lesson1;
     public class FirstApp {
         public static void main(String[] args) {
             System.out.println("Hello, World!");
-            int a = 10;
+//  Первое задание
+            byte a = 120;
+            short b = 20000;
+            int c = 345839;
+            long d = 9223372036854775807L;
+            float e = 7.67f;
+            double f = 3.45653445;
+            char g = '\u2242';
+            boolean h = true;
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(d);
+            System.out.println(e);
+            System.out.println(f);
+            System.out.println(g);
+            System.out.println(h);
+//  Второе задание
+
+
         }
     }
 
